5b60aa8b438473f659dcccd3365a76eded2538a6
Chirayu <myselfchirayu07@gmail.com>
2023-08-09 13:57:14 +0530

    Initial commit

---
+++ src/SetterGetterGenerator/SetterGetterGenerator.java
@@ -0,0 +1,111 @@
+package SetterGetterGenerator;
+import java.lang.reflect.*; // Including this for Reflection API
+import java.util.*; //Including this for to use Java Collection Classes
+import java.io.*; // Including this for file handling
+public class SetterGetterGenerator
+{
+public static void main(String cs[])
+{
+if(cs.length!=1 && cs.length!=2)
+{
+System.out.println("Usage : java classpath path_to_jar_file;. SetterGetterGenerator.SetterGetterGenerator class_name constructor=true/false");
+return;
+}
+if(cs.length==2)
+{
+if(cs[1].equalsIgnoreCase("constructor=true")==false && cs[1].equalsIgnoreCase("constructor=false")==false)
+{
+System.out.println("Usage : java classpath path_to_jar_file;. SetterGetterGenerator.SetterGetterGenerator class_name constructor=true/false");
+return;
+}
+}
+String className=cs[0];
+try
+{
+Class c=Class.forName(className);
+Field fields[]=c.getDeclaredFields();
+Field field;
+List<String> list=new ArrayList<String>();
+String fieldName;
+String setterName;
+String getterName;
+String tmp;
+String line;
+Class fieldType;
+if(cs.length==1 || (cs.length==2 && cs[1].equalsIgnoreCase("constructor=true")))
+{
+line="public "+c.getSimpleName()+"()";
+list.add(line);
+list.add("{");
+for(int e=0;e<fields.length;e++)
+{
+field=fields[e];
+line="this."+field.getName()+"="+getDefaultValue(field.getType())+";";
+list.add(line);
+}
+list.add("}");
+}
+for(int e=0;e<fields.length;e++)
+{
+field=fields[e];
+fieldName=field.getName();
+fieldType=field.getType();
+if(fieldName.charAt(0)>=97 && fieldName.charAt(0)<=122)
+{
+tmp=fieldName.substring(0,1).toUpperCase()+fieldName.substring(1);
+}
+else
+{
+tmp=fieldName;
+}
+setterName="set"+tmp;
+getterName="get"+tmp;
+line="public void "+setterName+"("+fieldType.getName()+" "+fieldName+")";
+list.add(line);
+list.add("{");
+line="this."+fieldName+"="+fieldName+";";
+list.add(line);
+list.add("}");
+line="public "+fieldType.getName()+" "+getterName+"()";
+list.add(line);
+list.add("{");
+line="return this."+fieldName+";";
+list.add(line);
+list.add("}");
+}
+File file=new File("tmp.tmp");
+if(file.exists()) file.delete();
+RandomAccessFile randomAccessFile;
+randomAccessFile=new RandomAccessFile(file,"rw");
+Iterator<String> iterator=list.iterator();
+while(iterator.hasNext())
+{
+line=iterator.next();
+randomAccessFile.writeBytes(line+"\r\n");
+}
+randomAccessFile.close();
+System.out.println("Setter/Getter for : "+c.getName()+" generated in file named as tmp.tmp");
+}catch(ClassNotFoundException cnfe)
+{
+System.out.println("Class : "+cnfe.getMessage()+ "Not found");
+}
+catch(IOException ioException)
+{
+System.out.println(ioException.getMessage());
+}
+}
+private static String getDefaultValue(Class c)
+{
+String className=c.getName();
+if(className.equals("java.lang.Long") || className.equals("long")) return "0";
+if(className.equals("java.lang.Integer") || className.equals("int")) return "0";
+if(className.equals("java.lang.Short") || className.equals("short")) return "0";
+if(className.equals("java.lang.Byte") || className.equals("byte")) return "0";
+if(className.equals("java.lang.Double") || className.equals("double")) return "0.0";
+if(className.equals("java.lang.Float") || className.equals("float")) return "0.0f";
+if(className.equals("java.lang.Character") || className.equals("char")) return "''";
+if(className.equals("java.lang.Boolean") || className.equals("boolean")) return "false";
+if(className.equals("java.lang.String")) return "\"\"";
+return "null";
+}
+}
